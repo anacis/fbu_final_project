e4f63d1826240db0c7dbd9c204db1c91483fcbce
Ana Cismaru <anacismaru@gmail.com>
2020-07-13 09:15:45 -0700

    initial xcode project commit

---
+++ Travelr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Travelr
+//
+//  Created by Ana Cismaru on 7/13/20.
+//  Copyright © 2020 anacismaru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Travelr/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Travelr
+//
+//  Created by Ana Cismaru on 7/13/20.
+//  Copyright © 2020 anacismaru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+

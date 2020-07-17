229ea49ead0d60742b6c9243d4c6d5d7cce04601
Ana Cismaru <anacismaru@gmail.com>
2020-07-16 18:18:36 -0700

    improved sorting algo by considering all starting points and picking the route with the smallest distance

--- Travelr/Models/PlaceList.m
+++ Travelr/Models/PlaceList.m
@@ -47,32 +47,42 @@
 
 //for now disregard time and days, just sort by distance assuming for one day
 - (NSArray *)sortPlaces {
+    NSMutableArray *bestSorted = [[NSMutableArray alloc] init];
+    double bestTotalDistance = INFINITY;
     
-    NSMutableArray *unsorted = [NSMutableArray arrayWithArray:self.placesUnsorted]; //make a copy as to not disturb orginal data
-    NSMutableArray *sorted = [[NSMutableArray alloc] init];
-    
-    //pick random starting point
-    //TODO: find better deterministic way to pick starting point
-    NSUInteger idx = arc4random() % unsorted.count;
-    [sorted addObject:unsorted[idx]];
-    [unsorted removeObjectAtIndex:idx];
-    
-    while (unsorted.count > 0) {
-        Place *from = sorted[sorted.count - 1];
-        Place *to;
-        double bestDistance = INFINITY;
-        for (Place *place in unsorted) {
-            double distance = [PlaceList getDistance:from place2:place];
-            if (distance < bestDistance) {
-                to = place;
-                bestDistance = distance;
+    for (int i = 0; i < self.placesUnsorted.count; i++) {
+        NSMutableArray *unsorted = [NSMutableArray arrayWithArray:self.placesUnsorted]; //make a copy as to not disturb orginal data
+        NSMutableArray *sorted = [[NSMutableArray alloc] init];
+        
+        [sorted addObject:unsorted[i]];
+        [unsorted removeObjectAtIndex:i];
+        
+        while (unsorted.count > 0) {
+            Place *from = sorted[sorted.count - 1];
+            Place *to;
+            double bestDistance = INFINITY;
+            for (Place *place in unsorted) {
+                double distance = [PlaceList getDistance:from place2:place];
+                if (distance < bestDistance) {
+                    to = place;
+                    bestDistance = distance;
+                }
             }
+        
+            [sorted addObject:to];
+            [unsorted removeObject:to];
         }
-    
-        [sorted addObject:to];
-        [unsorted removeObject:to];
+        
+        double totalDistance = [PlaceList getTotalDistance:sorted];
+        
+        if (totalDistance < bestTotalDistance) {
+            bestTotalDistance = totalDistance;
+            bestSorted = sorted;
+        }
+       
     }
-    return sorted;
+    
+    return bestSorted;
 }
 
 + (double)getDistance:(Place *)place1 place2:(Place *) place2 {
@@ -86,6 +96,14 @@
     //orthodromic distance formula in miles (units don't really matter I guess):
     double distance = 3963.0 * acos((sin(lat1) * sin(lat2)) + cos(lat1) * cos(lat2) * cos(long2 - long1));
     return distance;
+}
+
++ (double)getTotalDistance:(NSArray *)list {
+    double totalDistance = 0;
+    for (int i = 1; i < list.count; i++) {
+        totalDistance += [PlaceList getDistance:list[i - 1] place2:list[i]];
+    }
+    return totalDistance;
 }
 
 + (double)convertToRadians:(NSNumber *)num {

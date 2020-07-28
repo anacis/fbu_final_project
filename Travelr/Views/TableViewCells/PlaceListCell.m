28a7cddc581aa3d677dc149729985245e7774cdc
Ana Cismaru <anacismaru@gmail.com>
2020-07-28 09:10:50 -0700

    fixed like button bug

--- Travelr/Views/TableViewCells/PlaceListCell.m
+++ Travelr/Views/TableViewCells/PlaceListCell.m
@@ -30,6 +30,7 @@
     [self.image loadInBackground];
     
     if ([[PFUser currentUser][@"favoriteLists"] containsObject:self.placeList.objectId]) {
+        NSLog(@"%@", self.placeList.objectId);
         [self.likeButton setSelected:YES];
     }
 }
@@ -44,13 +45,14 @@
     PFUser *currUser = [PFUser currentUser];
     NSMutableArray *list = currUser[@"favoriteLists"];
     if (self.likeButton.isSelected) {
+        NSLog(@"Unliked %@", self.placeList.objectId);
         [self.likeButton setSelected:NO];
-        [list removeObject:self.placeList];
+        [list removeObject:self.placeList.objectId];
         
     }
     else {
         [self.likeButton setSelected:YES];
-        
+        NSLog(@"Liked %@", self.placeList.objectId);
         if (list == nil) {
             list = [[NSMutableArray alloc] init];
         }

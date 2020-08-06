bec185f1452cc2497d011be38f7c3b4cfffabc16
Ana Cismaru <anacismaru@gmail.com>
2020-08-06 12:15:50 -0700

    created colors file

---
+++ Travelr/Constants/Colors.h
@@ -0,0 +1,34 @@
+//
+//  Colors.h
+//  Travelr
+//
+//  Created by Ana Cismaru on 8/6/20.
+//  Copyright © 2020 anacismaru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Colors : UIColor
+
+//Travelr 1 Palette
++(UIColor *)lightGreenT1;
++(UIColor *)mediumGreenT1;
++(UIColor *)darkGreenT1;
++(UIColor *)lightOrangeT1;
++(UIColor *)darkOrangeT1;
+
+
+//Travelr 2 Palette
++(UIColor *)lightGreenT2;
++(UIColor *)mediumGreenT2;
++(UIColor *)darkGreenT2;
++(UIColor *)lightOrangeT2;
++(UIColor *)darkOrangeT2;
++(UIColor *)creamT2;
++(UIColor *)whiteT2;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Travelr/Constants/Colors.m
@@ -0,0 +1,57 @@
+//
+//  Colors.m
+//  Travelr
+//
+//  Created by Ana Cismaru on 8/6/20.
+//  Copyright © 2020 anacismaru. All rights reserved.
+//
+
+#import "Colors.h"
+
+@implementation Colors
+
+//Travelr 1
++(UIColor *)lightGreenT1 {
+    return [UIColor colorWithRed:104.0f/255.0f green:197.0f/255.0f blue:123.0f/255.0f alpha:1.0];
+}
+
++(UIColor *)mediumGreenT1 {
+    return [UIColor colorWithRed:64.0f/255.0f green:166.0f/255.0f blue:85.0f/255.0f alpha:1.0];
+}
+
++(UIColor *)darkGreenT1 {
+    return [UIColor colorWithRed:43.0f/255.0f green:87.0f/255.0f blue:64.0f/255.0f alpha:1.0];
+}
+
++(UIColor *)lightOrangeT1 {
+    return [UIColor colorWithRed:253.0f/255.0f green:172.0f/255.0f blue:98.0f/255.0f alpha:1.0];
+}
+
++(UIColor *)darkOrangeT1 {
+    return [UIColor colorWithRed:253.0f/255.0f green:141.0f/255.0f blue:98.0f/255.0f alpha:1.0];
+}
+
+//Travelr 2
++(UIColor *)lightGreenT2 {
+    return [UIColor colorWithRed:133.0f/255.0f green:174.0f/255.0f blue:139.0f/255.0f alpha:1.0];
+}
++(UIColor *)mediumGreenT2 {
+    return [UIColor colorWithRed:118.0f/255.0f green:157.0f/255.0f blue:122.0f/255.0f alpha:1.0];
+}
++(UIColor *)darkGreenT2 {
+    return [UIColor colorWithRed:81.0f/255.0f green:125.0f/255.0f blue:85.0f/255.0f alpha:1.0];
+}
++(UIColor *)lightOrangeT2 {
+    return [UIColor colorWithRed:237.0f/255.0f green:163.0f/255.0f blue:107.0f/255.0f alpha:1.0];
+}
++(UIColor *)darkOrangeT2 {
+    return [UIColor colorWithRed:238.0f/255.0f green:112.0f/255.0f blue:85.0f/255.0f alpha:1.0];
+}
++(UIColor *)creamT2 {
+    return [UIColor colorWithRed:233.0f/255.0f green:225.0f/255.0f blue:222.0f/255.0f alpha:1.0];
+}
++(UIColor *)whiteT2 {
+    return [UIColor colorWithRed:250.0f/255.0f green:251.0f/255.0f blue:250.0f/255.0f alpha:1.0];
+}
+
+@end
